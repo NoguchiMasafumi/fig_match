dff8b7013bcd7ec1829963da930622cd863c871c
NoguchiMasafumi <89947969+NoguchiMasafumi@users.noreply.github.com>
2022-05-20 08:06:28 +0900

    Add files via upload

--- js/head_menu.js
+++ js/head_menu.js
@@ -1,6 +1,6 @@
 document.write('<div class="head_menu bor_b_w1 shd2_9">\
     <div class="cont_cont mar_l_at mar_r_at ov_f_hid">\
-        <div class="flt_lef mar_r24"><a href="index.htm"><img src="img/ctl.png"></a></div>    \
+        <div class="flt_lef mar_r24"><a href="index.htm"><img src="img/ctl.png" style="height:36px;"></a></div>    \
         <div class="flt_lef mar_r24">select play</div>    \
         <div class="flt_lef mar_r24">shop</div>    \
         <div class="flt_lef mar_r24">save</div>    \
